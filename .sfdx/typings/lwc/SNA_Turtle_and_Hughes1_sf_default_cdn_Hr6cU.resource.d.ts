ecc9c1b09a0212c91114c1d2a79a9bfc1a5a295e
Josh Cathey <josh.cathey33@gmail.com>
2022-12-15 11:12:15 -0800

    TH-14

---
+++ .sfdx/typings/lwc/SNA_Turtle_and_Hughes1_sf_default_cdn_Hr6cU.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/SNA_Turtle_and_Hughes1_sf_default_cdn_Hr6cU" {
+    var SNA_Turtle_and_Hughes1_sf_default_cdn_Hr6cU: string;
+    export default SNA_Turtle_and_Hughes1_sf_default_cdn_Hr6cU;
+}
---
+++ .sfdx/typings/lwc/turtleLogo.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/turtleLogo" {
+    var turtleLogo: string;
+    export default turtleLogo;
+}
